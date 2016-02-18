77bed6e34a7d1e3a5aa6acba442a66884f20297f
Nick Peppers <nick.peppers@hitcents.com>
2016-02-18 13:52:10 -0600

    Initial Commit

---
+++ ImageTest.Forms/ImageTest.Forms/Pages/FirstPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ImageTest.Forms.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ImageTest.Forms.Pages
+{
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+
+            _nextButton.Clicked += OnNextPage;
+            _listView.ItemsSource = new string[] { "1", "2", "3" };
+        }
+
+        private async void OnNextPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
